7a12f57d71c9d3b8b1a0006136ec0f4efecb2fda
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2023-11-15 12:06:50 +0700

    Add Endpoint /transaction success

--- src/main/java/com/nutech/api/repository/ServiceRepository.java
+++ src/main/java/com/nutech/api/repository/ServiceRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface ServiceRepository extends CrudRepository<Service, Integer> {
-    @Query("SELECT s FROM Service s WHERE s.service_code = :service_code")
+    @Query("SELECT service_code FROM table_service WHERE service_code = :service_code")
     Service findByService_code(@Param("service_code") String service_code);
 }
